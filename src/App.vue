ede368bb134b13a73fb131841fad833d811f6eef
Sahil8470 <gupta2003.sahil@gmail.com>
2023-01-19 20:10:19 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import navbar from "./components/navbar.vue"
+  import blogview from "./components/blogview.vue"
+  import { ref } from 'vue';
+  const componentKey = ref(0);
+  const forceRerender = () => {
+    componentKey.value += 1;
+  };
+</script>
+
+<template>
+  <navbar :key="componentKey"/>
+  <blogview/>
+</template>
+
---
+++ src/components/navbar.vue
@@ -0,0 +1,41 @@
+<script setup>
+    import { ref } from 'vue'
+</script>
+
+
+<template>
+    <nav class="flex justify-start bg-[#0a0a23] font-montserrat">
+            <div class="pr-80 pl-40 pt-6 pb-4">
+                <a href="">
+                    <img
+                        class="h-10"
+                        src="../assets/images/TCD_Transparent_logo.png"
+                        alt="TheCyberDelta"
+                    />
+                </a>
+            </div>
+            <div class="flex justify-start pt-4 pb-4 text-white">
+                <div class="flex justify-center pt-2">
+                    <div class="pr-20 text-lg">
+                        <a href="">Blogs</a>
+                    </div>
+                    <div class="pr-20 text-lg">
+                        <a href="">About</a>
+                    </div>
+                    <div class="pr-20 text-lg">
+                        <a href="">Contact Us</a>
+                    </div>
+                    <div class="pr-16 text-lg">
+                        <a href="">Login</a>
+                    </div>
+                </div>
+                <div class="">
+                    <button
+                        class="text-white text-lg py-2 px-5 bg-[#27ae60] rounded-lg hover:bg-[#1e8449]"
+                    >
+                        Sign Up
+                    </button>
+                </div>
+            </div>
+        </nav>
+</template>
